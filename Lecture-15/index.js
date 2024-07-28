4e513c7781bf7fcf585f7b0a1e4c2d69cd3c5b3e
Raj Solanki <147243819+RajSolanki09@users.noreply.github.com>
2024-07-28 10:04:44 +0530

    index.js

--- Lecture-15/index.js
+++ Lecture-15/index.js
@@ -1,39 +1,44 @@
+let users = [];
 const handleData = (e) => {
     e.preventDefault();
 
     const user = {
-        name: document.getElementById('username'),
-        number: document.getElementById('number'),
-        email: document.getElementById('email'),
-        password: document.getElementById('password'),
-        fee: document.getElementById('fee')
+        name: document.getElementById('username').value,
+        number: document.getElementById('number').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        fee: document.getElementById('fee').value
     };
+
+  
+    if (user.name.length > 2) {
+        document.getElementById('username').style.border = '1px solid green';
+    } else {
+        document.getElementById('username').style.border = '2px solid red';
+    }
 
     // Indian mobile number regex
     let regexNumber = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
     if (regexNumber.test(user.number)) {
-        user.number.classList.remove('invalid');
+        document.getElementById('number').style.border = '1px solid green';
     } else {
-        user.number.classList.add('invalid');
-        return
+        document.getElementById('number').style.border = '2px solid red';
     }
 
     // Email regex
     let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (regexEmail.test(user.email)) {
-        user.email.classList.remove('invalid');
+        document.getElementById('email').style.border = '1px solid green';
     } else {
-        user.email.classList.add('invalid');
-        return
+        document.getElementById('email').style.border = '2px solid red';
     }
 
     // Password regex
     let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (regexPassword.test(user.password)) {
-        user.password.classList.remove('invalid');
+        document.getElementById('password').style.border = '1px solid green';
     } else {
-        user.password.classList.add('invalid');
-        return
+        document.getElementById('password').style.border = '2px solid red';
     }
 };
 
