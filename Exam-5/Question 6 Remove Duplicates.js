1d67e446d0d73f10f66cb24218fd8ba24ffb857d
Raj Solanki <147243819+RajSolanki09@users.noreply.github.com>
2024-07-30 17:33:08 +0530

    Question 6 Remove Duplicates.js

--- Exam-5/Question 6 Remove Duplicates.js
+++ Exam-5/Question 6 Remove Duplicates.js
@@ -1,10 +1,8 @@
 const Duplicates=()=>{
     var arr= [1, 2, 2, 3, 4, 4, 5]
     for (let i = 0; i < arr.length; i++) {
-        // Check if the element is already in the uniqueArray
-        if (uniqueArray.indexOf(arr[i]) === -1) {
-            // If it's not, add it to the uniqueArray
-            uniqueArray.push(arr[i]);
+        if (arr.[i](arr[i]) === -1) {
+            splice
         }
     }
     
